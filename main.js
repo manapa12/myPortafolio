35e3359f735253c44c01b9d3e2736f57b7a6c898
manuelapa <manuelr.aparicio@hotmail.com>
2023-09-16 00:47:58 -0600

    Added menu, and link for CV. Version 1.00 is now ready

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 let menuButton = document.getElementById("menu-button");
 let header = document.getElementById("my-header")
 let menu  = document.getElementById("my-menu")
+let myBody = document.getElementsByTagName("body");
+let cv = document.getElementById("cv-download")
 
 function handleClick() {
-    menu.style.display = "inline-block";
+    if(menu.style.display === "none"){
+      menu.style.display = "inline-block"
+    }else{
+        menu.style.display = "none"
+    }
 }
 
+function openLink() {
+    window.open("https://drive.google.com/drive/folders/1NyCyxZoRyDtnG7lVNNauCqKAT2aQbFUK?usp=sharing","_blank");
+}
 
+cv.addEventListener("click", openLink);
 menuButton.addEventListener("click",handleClick);
 
 
